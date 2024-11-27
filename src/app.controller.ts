c50f361d901e51cbc6009a0d5d07eac4bb0d2f74
Mamun Ahmed <mamunahmedusa@gmail.com>
2024-11-27 23:24:54 +0600

    initial

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,16 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/test')
+  getTest(): string {
+    return this.appService.test();
+  }
+  @Get('/test2')
+  getTest2(): string {
+    return 'test2';
   }
 }
